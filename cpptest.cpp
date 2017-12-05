94146f8deede6cee0817cba87086421ebf6c864a
mjwz5294 <393578079@qq.com>
2017-12-05 23:23:49 +0800

    helloworld

---
+++ cpptest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	cout << "---------------" << endl;
+	return 0;
+}
+
+
+
+
+
+/*
+1、g++ cpptest.cpp -o cpptest
+2、./cpptest
+*/
